f06849ec5734498281e0bcf3426aa2952f8071b4
Myke Pedro Paduelo <pedropaduelo@gmail.com>
2021-04-01 20:20:54 -0300

    Login

--- src/Layout/LoginCentral/Gmail/SignGmail.js
+++ src/Layout/LoginCentral/Gmail/SignGmail.js
@@ -1,66 +1,140 @@
-import React, { useContext } from 'react';
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-import { useHistory } from "react-router-dom";
-
-import GoogleLogin from 'react-google-login';
-
-import {makeStyles} from '@material-ui/core/styles';
-
-import { AuthContext } from '../../../Contexts/AuthContext';
-
-import api from '../../../services/api';
-
-
-const useStyles = makeStyles((theme) => ({
-  submit: {
-    margin: "5rem 0 3rem" ,
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-}));
-
-
-export default function SignInSide() {
-  const history = useHistory();
-  const classes = useStyles();
-
-  const { handleSignup, handleListFilasUser} = useContext(AuthContext);
-  
-  
-  const responseGoogle = (response) => {
-  }
-
-
-
-  async function Logar(response){
-    const tokenId = response.tokenId
-    const {data} = await api.post(`/CreateUser`, {tokenId})
-
-    const setContextUser =  await handleSignup(tokenId,data)
-                            await handleListFilasUser(data.user.email)
-
-    if(setContextUser){
-      history.push('/PainelGeral')
-    }
-  } 
-
-
-
-    
+function Copyright() {
   return (
-  
-    <div className={classes.submit}>
-      <GoogleLogin
-        clientId="457320718168-34ekq1e25neb1dnneg0g8qooq0udr6n3.apps.googleusercontent.com"
-        buttonText="Se cadastrar com Gmail"
-        theme={"dark"}
-        onSuccess={Logar}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-      />
-    </div>
-
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
   );
 }
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignUp() {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,8 @@ import React from "react";
 import { Switch } from "react-router-dom";
 import CustomRoute from './CustomRoute'
 
-import LoginLateral from "../Layout/LoginLateral/Gmail";
+import LoginCentralLogin from "../Layout/LoginCentral/Jwt/Login";
+import LoginCentralSign from "../Layout/LoginCentral/Jwt/Sign";
 import Dashboard from "../Layout/Dashboard";
 
 
@@ -12,7 +13,9 @@ function Routes(){
   return(
     <Switch>
 
-      <CustomRoute exact path="/" redirectTO={"/"} component={LoginLateral} /> 
+      <CustomRoute exact path="/" redirectTO={"/"} component={LoginCentralLogin} /> 
+      <CustomRoute exact path="/Sign" redirectTO={"/"} component={LoginCentralSign} /> 
+
       <CustomRoute isPrivate path="/Painel" redirectTO={"/"} component={Dashboard} /> 
 
     </Switch>
